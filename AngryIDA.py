677109a35ab4ac4a0bf27de21781276e3c8fae96
Brandon-Everhart <bte14@my.fsu.edu>
2017-11-22 21:17:56 -0500

    Code rewrite and update. README update.

--- AngryIDA.py
+++ AngryIDA.py
@@ -1,177 +1,242 @@
-#----------------------------------------------------------------------------
-# angr plug-in for IDA Pro
-#
-#
-#
-#----------------------------------------------------------------------------
+'''
+angr plug-in for IDA Pro.
+
+Integrates point and click use of the angr binary analysis framework
+inside of IDA Pro. The plug-in adds a sub menu, called AngryIDA, to
+IDA View-A's pop-up context menu. Inside the IDA View-A window
+right-click and expand the AngryIDA menu item to use.
+'''
 from __future__ import print_function
-import idaapi, angr
+import angr
+import idaapi #pylint: disable=import-error
 
+FIND_ADDRS = []
+AVOID_ADDRS = []
 
-findAddrs = []
-avoidAddrs = []
+class ActionHandler(idaapi.action_handler_t):
+    """
+    TODO
+    """
+    def __init__(self, action):
+        """
+        TODO
+        """
+        idaapi.action_handler_t.__init__(self)
+        self.action = action
 
-def hotkey_pressed_find():
-	try:
-		addr = idaapi.get_screen_ea()
-		if addr in findAddrs:
-			findAddrs.remove(addr)
-			SetColor(here(), CIC_ITEM, 0xffffff)
-			print("IDA~ANGR: Removed find address [%s]" % hex(addr))
-		else:
-			if addr in avoidAddrs:
-				avoidAddrs.remove(addr)
-			findAddrs.append(addr)
-			SetColor(here(), CIC_ITEM, 0x208020)
-			print("IDA~ANGR: Added find address [%s]" % hex(addr))
-	except:
-		print("IDA~ANGR:\n*** FAILED TO HANDLE FIND ADDRESS ***\n") 
+    def activate(self, ctx):
+        """
+        TODO
+        """
+        if self.action == "Finds:Set":
+            find_set()
+        elif self.action == "Finds:Remove":
+            find_remove()
+        elif self.action == "Finds:Print":
+            find_view()
+        elif self.action == "Avoids:Set":
+            avoid_set()
+        elif self.action == "Avoids:Remove":
+            avoid_remove()
+        elif self.action == "Avoids:Print":
+            avoid_view()
+        elif self.action == "Explore:Run":
+            explore()
+        elif self.action == "Refresh:Refresh":
+            refresh()
+        elif self.action == "Quit:Quit":
+            my_quit()
 
-def hotkey_pressed_avoid():
-	try:
-		addr = idaapi.get_screen_ea()
-		if addr in avoidAddrs:
-			avoidAddrs.remove(addr)
-			SetColor(here(), CIC_ITEM, 0xffffff)
-			print("IDA~ANGR: Removed avoid address [%s]" % hex(addr))
-		else:
-			if addr in findAddrs:
-				findAddrs.remove(addr)
-			avoidAddrs.append(addr)
-			SetColor(here(), CIC_ITEM, 0x2020c0)
-			print("IDA~ANGR: Added avoid address [%s]" % hex(addr))
-	except:
-		print("IDA~ANGR:\n*** FAILED TO HANDLE AVOID ADDRESS ***\n") 
+    def update(self, ctx):
+        """
+        TODO
+        """
+        return idaapi.AST_ENABLE_ALWAYS
 
+class Hooks(idaapi.UI_Hooks):
+    """
+    TODO
+    """
+    def finish_populating_tform_popup(self, form, popup):
+        """
+        TODO
+        """
+        if idaapi.get_tform_title(form) == "IDA View-A":
+            idaapi.attach_action_to_popup(form, popup, "Finds:Set", "AngryIDA/Finds/")
+            idaapi.attach_action_to_popup(form, popup, "Finds:Remove", "AngryIDA/Finds/")
+            idaapi.attach_action_to_popup(form, popup, "Finds:Print", "AngryIDA/Finds/")
+            idaapi.attach_action_to_popup(form, popup, "Avoids:Set", "AngryIDA/Avoids/")
+            idaapi.attach_action_to_popup(form, popup, "Avoids:Remove", "AngryIDA/Avoids/")
+            idaapi.attach_action_to_popup(form, popup, "Avoids:Print", "AngryIDA/Avoids/")
+            idaapi.attach_action_to_popup(form, popup, "Explore:Run", "AngryIDA/Explore/")
+            idaapi.attach_action_to_popup(form, popup, "Refresh:Refresh", "AngryIDA/")
+            idaapi.attach_action_to_popup(form, popup, "Quit:Quit", "AngryIDA/")
 
-def hotkey_pressed_view():
-	try:
-		print("IDA~ANGR:\n\tFind Addresses")
-		for addr in findAddrs:
-			print("\t\t%s" % hex(addr))
+def set_line_color(color, addr=here(), item=CIC_ITEM): #pylint: disable=undefined-variable
+    """
+    Function: set_line_color
+    Arguments:
+        Mandatory:
+            - ( Name: color,
+                Position: 0,
+                Type: int,
+                Value: hex/int color value )
+        Optional:
+            - ( Name: addr,
+                Position: 1,
+                Type: int,
+                Default: here(),
+                Value: hex/int memory address )
+            - ( Name: item,
+                Position: 2,
+                Type: IDA Item,
+                Default: CIC_ITEM,
+                Value: IDA Item )
+    Return: None
+    Description:
+        Change instruction line color in IDA with specified color and address.
+        Disabled pylint errors due to IDA function calls.
+    TODO:
+        Nothing currently.
+    """
+    SetColor(addr, item, color) #pylint: disable=undefined-variable
 
-		print("\tAvoid Addresses")
-		for addr in avoidAddrs:
-			print("\t\t%s" % hex(addr))
-		
-	except:
-		print("IDA~ANGR:\n*** FAILED TO VIEW VARIABLES ***\n") 
-		
-def hotkey_pressed_refresh():
-	try:
-		for addr in findAddrs:
-			SetColor(addr, CIC_ITEM, 0xffffff)
-			findAddrs.remove(addr)
-			
-		for addr in avoidAddrs:
-			SetColor(addr, CIC_ITEM, 0xffffff)
-			avoidAddrs.remove(addr)
+def find_set():
+    """
+    Arguments: None
+    Return Value: None
+    Description:
+        - Toggles find address in list and color on screen.
+    TODO:
+        - Better description
+    """
+    addr = idaapi.get_screen_ea()
+    if addr in AVOID_ADDRS:
+        AVOID_ADDRS.remove(addr)
+        print("AngryIDA: Removed avoid address [%s]" % hex(addr))
+    FIND_ADDRS.append(addr)
+    set_line_color(0x208020, addr)
+    print("AngryIDA: Added find address [%s]" % hex(addr))
 
-		print("IDA~ANGR: Refresh completed.")
+def find_remove():
+    """
+    Arguments: None
+    Return Value: None
+    Description:
+        - Toggles find address in list and color on screen.
+    TODO:
+        - Better description
+    """
+    addr = idaapi.get_screen_ea()
+    if addr in FIND_ADDRS:
+        FIND_ADDRS.remove(addr)
+        set_line_color(0xffffff, addr)
+        print("AngryIDA: Removed find address [%s]" % hex(addr))
 
-	except:
-		print("IDA~ANGR:\n*** FAILED TO REFRESH ***\n")
+def find_view():
+    '''
+    TODO
+    '''
+    print("AngryIDA:\n\tFind Addresses")
+    for addr in FIND_ADDRS:
+        print("\t\t%s" % hex(addr))
 
+def avoid_set():
+    """
+    TODO
+    """
+    addr = idaapi.get_screen_ea()
+    if addr in FIND_ADDRS:
+        FIND_ADDRS.remove(addr)
+        print("AngryIDA: Removed find address [%s]" % hex(addr))
+    AVOID_ADDRS.append(addr)
+    set_line_color(0x2020c0, addr)
+    print("AngryIDA: Added avoid address [%s]" % hex(addr))
 
-def hotkey_pressed_explore():
-	print("Hotkey explore has been pressed!")
-	file = idaapi.get_input_file_path()
-	print(file)
-	proj = angr.Project(file,  load_options={"auto_load_libs":False})
-	initial_state = proj.factory.entry_state() 
-	initial_state.options.discard("LAZY_SOLVES")
+def avoid_remove():
+    """
+    TODO
+    """
+    addr = idaapi.get_screen_ea()
+    if addr in AVOID_ADDRS:
+        AVOID_ADDRS.remove(addr)
+        set_line_color(0xffffff, addr)
+        print("AngryIDA: Removed avoid address [%s]" % hex(addr))
 
-	for _ in range(0,32):
-		k = initial_state.posix.files[0].read_from(1)
-		initial_state.se.add(k != 0)
-		initial_state.se.add(k != 10)
+def avoid_view():
+    '''
+    TODO
+    '''
+    print("\tAvoid Addresses")
+    for addr in AVOID_ADDRS:
+        print("\t\t%s" % hex(addr))
 
-	# The last char of user input must be a newline
-	k = initial_state.posix.files[0].read_from(1)
-	initial_state.se.add(k == 10)
+def explore():
+    """
+    TODO
+    """
+    binary_file = idaapi.get_input_file_path()
+    proj = angr.Project(binary_file, load_options={"auto_load_libs":False})
+    initial_state = proj.factory.entry_state()
+    initial_state.options.discard("LAZY_SOLVES")
+    for _ in range(0, 32):
+        k = initial_state.posix.files[0].read_from(1)
+        initial_state.se.add(k != 0)
+        initial_state.se.add(k != 10)
 
-	# Reset the stdin to the beginning, 0
-	initial_state.posix.files[0].seek(0)
-	initial_state.posix.files[0].length = 33
+    # The last char of user input must be a newline
+    k = initial_state.posix.files[0].read_from(1)
+    initial_state.se.add(k == 10)
 
-	pg = proj.factory.path_group(initial_state, immutable=False)
-	pg.explore(find=findAddrs, avoid=avoidAddrs)
+    # Reset the stdin to the beginning, 0
+    initial_state.posix.files[0].seek(0)
+    initial_state.posix.files[0].length = 33
 
-	found = pg.found[0].state
-	found.posix.files[0].seek(0)
-	print("Found: "+ found.se.any_str(found.posix.files[0].read_from(33)))
+    path_group = proj.factory.path_group(initial_state, immutable=False)
+    path_group.explore(find=FIND_ADDRS, avoid=AVOID_ADDRS)
 
+    found = path_group.found[0].state
+    found.posix.files[0].seek(0)
+    print("Found: "+ found.se.any_str(found.posix.files[0].read_from(33)))
 
+def refresh():
+    """
+    TODO
+    """
+    print(FIND_ADDRS, AVOID_ADDRS)
+    for addr in FIND_ADDRS:
+        set_line_color(0xffffff, addr)
+    del FIND_ADDRS[:]
+    for addr in AVOID_ADDRS:
+        set_line_color(0xffffff, addr)
+    del AVOID_ADDRS[:]
+    print("AngryIDA: Refresh completed.")
 
+def my_quit():
+    """
+    TODO
+    """
+    return 1
 
-try: 
-	hotkey_find	
-	if idaapi.del_hotkey(hotkey_find):
-		print("Hotkey find unregistered!")
-		del hotkey_find
-	else:
-		print("Failed to delete hotkey find!")
+ACTION_FS = idaapi.action_desc_t('Finds:Set', 'Set', ActionHandler("Finds:Set"))
+ACTION_FR = idaapi.action_desc_t('Finds:Remove', 'Remove', ActionHandler("Finds:Remove"))
+ACTION_FP = idaapi.action_desc_t('Finds:Print', 'Print', ActionHandler("Finds:Print"))
+ACTION_AS = idaapi.action_desc_t('Avoids:Set', 'Set', ActionHandler("Avoids:Set"))
+ACTION_AR = idaapi.action_desc_t('Avoids:Remove', 'Remove', ActionHandler("Avoids:Remove"))
+ACTION_AP = idaapi.action_desc_t('Avoids:Print', 'Print', ActionHandler("Avoids:Print"))
+ACTION_ER = idaapi.action_desc_t('Explore:Run', 'Run', ActionHandler("Explore:Run"))
+ACTION_RR = idaapi.action_desc_t('Refresh:Refresh', 'Refresh', ActionHandler("Refresh:Refresh"))
+ACTION_QQ = idaapi.action_desc_t('Quit:Quit', 'Quit', ActionHandler("Quit:Quit"))
 
-	hotkey_avoid	
-	if idaapi.del_hotkey(hotkey_avoid):
-		print("Hotkey avoid unregistered!")
-		del hotkey_avoid
-	else:
-		print("Failed to delete hotkey avoid!")
+idaapi.register_action(ACTION_FS)
+idaapi.register_action(ACTION_FR)
+idaapi.register_action(ACTION_FP)
+idaapi.register_action(ACTION_AS)
+idaapi.register_action(ACTION_AR)
+idaapi.register_action(ACTION_AP)
+idaapi.register_action(ACTION_ER)
+idaapi.register_action(ACTION_RR)
+idaapi.register_action(ACTION_QQ)
 
-	hotkey_view
-	if idaapi.del_hotkey(hotkey_view):
-		print("Hotkey view unregistered!")
-		del hotkey_view
-	else:
-		print("Failed to delete hotkey view!")
-
-	hotkey_refresh
-	if idaapi.del_hotkey(hotkey_refresh):
-		print("Hotkey refresh unregistered!")
-		del hotkey_refresh
-	else:
-		print("Failed to delete hotkey refresh!")
-
-	hotkey_explore
-	if idaapi.del_hotkey(hotkey_explore):
-		print("Hotkey explore unregistered!")
-		del hotkey_explore
-	else:
-		print("Failed to delete hotkey explore!")
-
-except:
-	hotkey_find = idaapi.add_hotkey("Ctrl-Shift-f", hotkey_pressed_find)
-	if hotkey_find is None:
-		print("Failed to register hotkey_find!")
-		del hotkey_find
-	else:
-		print("Hotkey_find registered! Ctrl-Shift-F")
-
-	hotkey_avoid = idaapi.add_hotkey("Ctrl-Shift-a", hotkey_pressed_avoid)
-	if hotkey_avoid is None:
-		print("Failed to register hotkey_avoid!")
-		del hotkey_avoid
-	else:
-		print("hotkey_avoid registered! Ctrl-Shift-A")
-
-	hotkey_view = idaapi.add_hotkey("Ctrl-Shift-v", hotkey_pressed_view)
-	if hotkey_view is None:
-		print("Failed to register hotkey_view!")
-		del hotkey_view
-	else:
-		print("hotkey_view registered! Ctrl-Shift-V")
-
-	hotkey_refresh = idaapi.add_hotkey("Ctrl-Shift-r", hotkey_pressed_refresh)
-	if hotkey_refresh is None:
-		print("Failed to register hotkey_refresh!")
-		del hotkey_refresh
-	else:
-		print("hotkey_refresh registered! Ctrl-Shift-R")
-
-	hotkey_explore = idaapi.add_hotkey("Ctrl-Shift-e", hotkey_pressed_explore)
-	if hotkey_explore is None:
-		print("Failed to register hotkey_explore!")
-		del hotkey_explore
-	else:
-		print("hotkey_explore registered! Ctrl-Shift-E")
+HOOKS = Hooks()
+HOOKS.hook()
--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # AngryIDA
+
+![pylint Score](https://mperlet.de/pybadge/badges/9.61.svg) [![standard-readme compliant](https://img.shields.io/badge/readme%20style-standard-brightgreen.svg)](https://github.com/RichardLitt/standard-readme)  ![conduct](https://img.shields.io/badge/code%20of%20conduct-contributor%20covenant-brightgreen.svg) 
+
+| Table of Contents |
+|-------------------|
+|[Background](#background)|
+|[Install](#install-tips)|
+|[Usage](#usage)|
+|[TODO](#todo)|
+|[Standards](#standards)|
+|[Maintainers](#maintainers)|
+|[Contribute](#contribute)|
+|[License](#license)|        
+
+## Background
+
+The goal of this plug-in is to integrate the use of the angr binary analysis framework into IDA Pro. 
+
+## Install Tips
+
+_**NOTE: This section only describes the process of installation and setup in our specific environment.**_
+
+**1. Windows 7 64 bit Virtual Machine?**
+- Yes: Cool you are in the same place as us.
+- No: Well.... no promises. 
+
+**2. IDA Pro 6.9?**
+- Yes: Install Microsoft Visual C++ 2015 or newer and update the .NET Framework.
+- No: Install IDA 6.9 then return here.
+
+**3. IDA Python Plug-In Working?**
+- Yes: Continue to the next step.
+- No: Sorry, we did not have this problem.
+
+**4. Python 2.7 is installed?**
+- Yes: Keep going.
+- No: There are many resources online for installing python 2.7 on virtually every system.
+
+**5. Python package manager pip is installed?**
+- Yes: Awesome, one step closer.
+- No: Installing pip is straightforward and help can be found online.
+
+**6. angr is installed and working?**
+- Yes: You got off easy! Skip to the next step.
+- No: Try this...
+    + Install Microsoft Visual Studio 2017 (Really you just need the developer command prompt)
+    + Install the Microsoft Visual C++ Compiler for Python 2.7
+    + Inside the Microsoft Visual Studio Developers Command Prompt run the following commands:
+        * pip install -I --pre --no-use-wheel capstone-windows
+        * pip install pyvex
+        * pip install unicorn
+        * pip install simuvex
+        * pip install angr
+
+**7. Download this repository (At least the file AngryIDA.py)?**
+- Yes: You are ready to use AngryIDA!
+- No: Why not?
+
+## Usage
+
+## TODO
+
+## Standards
+
+* [Standard Readme](https://github.com/RichardLitt/standard-readme)
+* [Contributor Covenant](https://contributor-covenant.org/version/1/3/0/) Code of Conduct.
+* [Pylint](https://www.pylint.org/)
+
+## Maintainers
+
+* [Brandon Everhart](https://github.com/Brandon-Everhart)
+* [Taylor Shields](https://github.com/Taylor-Shields)
+
+## Contribute
+
+Any and all contributions are appertained! [Open an issue](https://github.com/Brandon-Everhart/AngryIDA/issues/new) or submit PRs.
+
+## Related Efforts
+
+* [Ponce](https://github.com/illera88/Ponce)
+
+## License
+[GNU General Public License v3.0](LICENSE)
